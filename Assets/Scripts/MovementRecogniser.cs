d1682e2d9f44baac8ad680d14961d244cceb14a0
Johnathan-Wang <S10257852@connect.np.edu.sg>
2025-01-27 12:47:25 +0800

    Movement creation and recognition
    Test version

--- Assets/Scripts/MovementRecogniser.cs
+++ Assets/Scripts/MovementRecogniser.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Hands;
 using UnityEngine.XR.Hands.Gestures;
+using PDollarGestureRecognizer;
+using System.IO;
+using UnityEngine.Events;
 
 public class MovementRecogniser : MonoBehaviour
 {
     public XRNode inputSource; // Specify LeftHand or RightHand
     public Transform movementSource;
     public GameObject debugCubePrefab;
+    public bool creationMode = false;
+    public string newGestureName;
+    
+    public float recognitionThreshold = 0.9f;
+
+    [System.Serializable]
+    public class UnityStringEvent : UnityEvent<string> { }
+    public UnityStringEvent OnGestureRecognised;
 
     public XRHandPose requiredPose; // Reference to the XRHandPose asset
     public float newPositionThresholdDistance = 0.05f;
 
+    private List<Gesture> trainingSet = new List<Gesture>();
     private XRHandSubsystem handSubsystem; // Hand tracking subsystem
     private bool isMoving = false;
     private List<Vector3> positionsList = new List<Vector3>();
 
     void Start()
     {
+        string[] gestureFiles = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+
+        foreach (var file in gestureFiles)
+        {
+            trainingSet.Add(GestureIO.ReadGestureFromFile(file));
+        }
+
         // Initialize the XRHandSubsystem
         var subsystems = new List<XRHandSubsystem>();
         SubsystemManager.GetInstances(subsystems);
@@ -93,6 +112,37 @@ public class MovementRecogniser : MonoBehaviour
     {
         isMoving = false;
         Debug.Log("End moving");
+
+        //Create Gesture From Position
+        Point[] pointArray = new Point[positionsList.Count];
+        for (int i = 0; i < positionsList.Count; i++)
+        {
+            Vector2 screenPosition = Camera.main.WorldToScreenPoint(positionsList[i]);
+            pointArray[i] = new Point(positionsList[i].x, positionsList[i].y, 0);
+        }
+
+        Gesture newGesture = new Gesture(pointArray);
+
+        //Add New Gesture to Training Set
+        if (creationMode)
+        {
+            newGesture.Name = newGestureName;
+            trainingSet.Add(newGesture);
+
+            //Save Training Set
+            string fileName = Application.persistentDataPath + "/" + newGestureName + ".xml";
+            GestureIO.WriteGesture(pointArray, newGestureName, fileName);
+        }
+        //Recognise Gesture
+        else
+        {
+            Result result = PointCloudRecognizer.Classify(newGesture, trainingSet.ToArray());
+            Debug.Log("Gesture Recognised: " + result.GestureClass + " " + result.Score);
+            if (result.Score > recognitionThreshold)
+            {
+                OnGestureRecognised.Invoke(result.GestureClass);
+            }
+        }
     }
 
     private void UpdateMovement()
