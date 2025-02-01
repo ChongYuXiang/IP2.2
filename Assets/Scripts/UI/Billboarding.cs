8a339ab86ad76c5d84a8f06319e55d7e03f6d9ee
yeji <sungyejoy@gmail.com>
2025-02-01 17:29:32 +0800

    ui changes
    billboarding, new font, ui fbx

---
+++ Assets/Scripts/UI/Billboarding.cs
@@ -0,0 +1,51 @@
+/*
+* Author: https://www.youtube.com/watch?v=eiGvVgwtJ8k
+* Date: 02/01/2024
+* Description: This script is to handle billboarding of all the UI
+* */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboarding : MonoBehaviour
+{
+    [SerializeField] private BillboardType billboardType;
+
+    [Header("Lock Rotation")]
+    [SerializeField] private bool lockX;
+    [SerializeField] private bool lockY;
+    [SerializeField] private bool lockZ;
+
+    private Vector3 originalRotation;
+
+    public enum BillboardType { LookAtCamera, CameraForward };
+
+    private void Awake()
+    {
+        originalRotation = transform.rotation.eulerAngles;
+    }
+
+    // Use Late update so everything should have finished moving.
+    void LateUpdate()
+    {
+        // There are two ways people billboard things.
+        switch (billboardType)
+        {
+            case BillboardType.LookAtCamera:
+                transform.LookAt(Camera.main.transform.position, Vector3.up);
+                break;
+            case BillboardType.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            default:
+                break;
+        }
+        // Modify the rotation in Euler space to lock certain dimensions.
+        Vector3 rotation = transform.rotation.eulerAngles;
+        if (lockX) { rotation.x = originalRotation.x; }
+        if (lockY) { rotation.y = originalRotation.y; }
+        if (lockZ) { rotation.z = originalRotation.z; }
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
