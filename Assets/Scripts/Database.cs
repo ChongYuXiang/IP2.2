be9eddde431a78790160c352f07abad6a0983998
ChongYuXiang <s10257151@connect.np.edu.sg>
2025-02-04 09:44:53 +0800

    Adjust database script
    Added comments and fixed email reset feedback

--- Assets/Scripts/Database.cs
+++ Assets/Scripts/Database.cs
@@ -24,9 +24,6 @@ public class Database : MonoBehaviour
     DatabaseReference dataRef;
     FirebaseAuth auth;
 
-    [SerializeField]
-    private GameObject UiManager;
-
     private string email;
     private string password;
     private string uuid;
@@ -92,6 +89,7 @@ public class Database : MonoBehaviour
         dataRef.Child("players").Child(uuid).SetRawJsonValueAsync(json);
         Debug.Log(json);
 
+        /*
         AlphabetGame alphabetgame = new AlphabetGame(0, 0, 0, 0, 0);
         json = JsonUtility.ToJson(alphabetgame);
         dataRef.Child("alphabet_game").Child(uuid).Child(timestamp).SetRawJsonValueAsync(json);
@@ -101,6 +99,7 @@ public class Database : MonoBehaviour
         json = JsonUtility.ToJson(wordgame);
         dataRef.Child("word_game").Child(uuid).Child(timestamp).SetRawJsonValueAsync(json);
         Debug.Log(json);
+        */
 
         ReadPlayerData();
     }
@@ -110,19 +109,20 @@ public class Database : MonoBehaviour
     {
         FirebaseDatabase.DefaultInstance.GetReference("players/" + uuid).GetValueAsync().ContinueWithOnMainThread(task =>
         {
-            if (task.IsFaulted)
+            if (task.IsFaulted) // If read fails
             {
                 Debug.Log("error");
                 return;
             }
-            else if (task.IsCompleted)
+            else if (task.IsCompleted) // If read successfull
             {
-                DataSnapshot snapshot = task.Result;
+                DataSnapshot snapshot = task.Result; // Take snapshot of data
 
-                string playerDetails = snapshot.GetRawJsonValue();
+                string playerDetails = snapshot.GetRawJsonValue(); // Convert snapshot to json
                 Debug.Log("raw json data of player" + playerDetails);
 
                 Player p = JsonUtility.FromJson<Player>(playerDetails);
+                // Save data
                 username = p.username;
                 email = p.email;
                 gender = p.gender;
@@ -166,18 +166,18 @@ public class Database : MonoBehaviour
 
         auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
         {
-            if (task.IsFaulted)
+            if (task.IsFaulted) // If log in fails
             {
                 Debug.Log("SignInilAndPasswordAsync error " + task.Exception);
-                errorText1.text = "Error: Check that email and password are input correctly";
+                errorText1.text = "Error: Check that email and password are input correctly"; // Display error text
             }
-            if (task.IsCompleted)
+            if (task.IsCompleted) // If lon in is successfull
             {
                 AuthResult result = task.Result;
                 Debug.LogFormat("User logged in successfully: {0} {1}", result.User.Email, result.User.UserId);
-                errorText1.text = "";
-                uuid = result.User.UserId;
-                ReadPlayerData();
+                errorText1.text = ""; // Hide error text
+                uuid = result.User.UserId; // Save uuid
+                ReadPlayerData(); // Read to retrieve data
             }
         });
     }
@@ -199,45 +199,49 @@ public class Database : MonoBehaviour
 
         auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
         {
-            if (task.IsFaulted)
+            if (task.IsFaulted) // If account creation failed
             {
                 Debug.LogFormat("CreateUserWithEmailAndPasswordAsync error " + task.Exception);
-                errorText2.text = "Error: Check that all fields have been input correctly";
+                errorText2.text = "Error: Check that all fields have been input correctly"; // Display error text
             }
-            else if (task.IsCompleted)
+            else if (task.IsCompleted) // If account creation is successfull
             {
                 AuthResult result = task.Result;
                 Debug.LogFormat("Firebase user created successfully: {0} {1}", result.User.Email, result.User.UserId);
-                errorText2.text = "";
-                uuid = result.User.UserId;
-                WriteNewPlayer(username, email, gender, race, true);
+                errorText2.text = ""; // Hide error text
+                uuid = result.User.UserId; // Save uuid
+                WriteNewPlayer(username, email, gender, race, true); // Write player with sign up data
             }
         });
     }
 
     public void SignOutPlayer()
     {
-        auth.SignOut();
+        // Set player's status to not active
+        Dictionary<string, object> childUpdates = new Dictionary<string, object>();
+        childUpdates[uuid + "/active_status"] = false;
+        FirebaseDatabase.DefaultInstance.GetReference("players").UpdateChildrenAsync(childUpdates);
+
+        auth.SignOut(); // Sign out user
+
         Debug.Log("Signed out");
     }
 
     public void forgotPassword()
     {
-        email = inputEmailReset.text;
+        email = inputEmailReset.text; // Retrieve email from input field
 
         auth.SendPasswordResetEmailAsync(email).ContinueWith(task => {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("SendPasswordResetEmailAsync was canceled.");
-                resetText.text = "Error: Email not found";
-            }
-            else if (task.IsFaulted)
+            if (task.IsFaulted) // If email cannot be sent
             {
                 Debug.LogError("SendPasswordResetEmailAsync encountered an error: " + task.Exception);
-                resetText.text = "Email sent successfully";
+                resetText.text = "Error: Email not found"; // Display error text
             }
-
-            Debug.Log("Password reset email sent successfully");
+            else if (task.IsCompleted) // If email is sent successfully
+            {
+                Debug.Log("Password reset email sent successfully");
+                resetText.text = "Email sent successfully"; // Display confirmation text
+            }
         });
     }
 }
