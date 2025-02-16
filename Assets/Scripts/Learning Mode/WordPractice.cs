eabbf777daf17f00140e996d016d005203b562bd
Johnathan-Wang <S10257852@connect.np.edu.sg>
2025-02-17 03:18:15 +0800

    Revert "Merge branch 'main' of https://github.com/ChongYuXiang/IP2.2"
    This reverts commit 7d9d1051f2e2a77a538d7f07854a6d58e4f08cda, reversing
    changes made to d053cb93c1486d0cce3facd17da95bd4fb58246f.

--- Assets/Scripts/Learning Mode/WordPractice.cs
+++ Assets/Scripts/Learning Mode/WordPractice.cs
@@ -15,6 +15,7 @@ public class WordPractice : MonoBehaviour
     // Learning UI elements
     private int progress = 0;
     public TextMeshPro wordDisplay;
+    //public ApplyTextureToPanel wordExampleVid;
     public TMP_InputField inputDisplay;
     public Image progressBar;
     public Button nextButton;
@@ -27,10 +28,6 @@ public class WordPractice : MonoBehaviour
     // Confetti VFX
     public ParticleSystem confetti;
 
-    // End popup
-    public GameObject endPopup;
-    public GameObject popupPage;
-
     // Hands color swapping
     public GameObject leftHand;
     public GameObject rightHand;
@@ -42,7 +39,7 @@ public class WordPractice : MonoBehaviour
     {
         if (GameManager.instance.wordsComplete == true) // Check GameManager if numbers are already unlocked
         {
-            progress = wordList.Count;
+            progress = 11;
         }
 
         if (wordList.Count == 0)
@@ -59,51 +56,28 @@ public class WordPractice : MonoBehaviour
 
         currentWord = wordList[nextWordIndex];
         wordDisplay.text = currentWord; // Display current word
+        //wordExampleVid.SendMessage("ChangeDisplay", currentWord.ToString()); // Display example sign video
 
         nextButton.onClick.AddListener(GetNewWord); // Set up the button to call GenNewWord
         nextButton.gameObject.SetActive(false); // Hide the next button at the start
 
-        GetNewWord(); // Get the first word
+        GetNewWord();
     }
-
-    public void PlayConfetti()
-    {
-        confetti.Play(); // Trigger confetti effect
-    }
-
 
     void GetNewWord()
     {
-        nextButton.gameObject.SetActive(false); // Hide the next button when generating a new word (Delete line for testing)
-
-        if (wordList.Count > nextWordIndex) // If next word if within the list
+        if (wordList.Count >= nextWordIndex)
         {
             currentWord = wordList[nextWordIndex];
-            wordDisplay.text = currentWord; // Display current word
+            wordDisplay.text = currentWord;
         }
         else
         {
-            nextWordIndex = 0; // Go back to first word
+            nextWordIndex = 0;
             currentWord = wordList[nextWordIndex];
-            wordDisplay.text = currentWord; // Display current word
+            wordDisplay.text = currentWord;
         }
-        nextWordIndex += 1; // Increment word count
-
-        wordDisplay.text = currentWord.ToString();
-
-        progress += 1;
-        progressBar.fillAmount = (float)progress / wordList.Count; // Update progress bar
-        if (progress >= 24 && GameManager.instance.numbersUnlocked == false) // If progress is complete, unlock number mode
-        {
-            PlayConfetti(); // Trigger confetti when game ends
-            AudioManager.instance.PlaySFX("Confetti");
-
-            GameManager.instance.numbersUnlocked = true;
-
-            // Show popup
-            endPopup.SetActive(true);
-            popupPage.SetActive(true);
-        }
+        nextWordIndex += 1;
     }
 
     public void ValidateWord()
